283dbcb63147a4c234e34b725a7e71ebb153dd9a
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-12-14 15:15:48 +0100

    Update SAFEPD_descr.R

--- SAFEPD_descr.R
+++ SAFEPD_descr.R
@@ -1,3 +1,44 @@
+# Run descriptive analyses for the data obtained via questionnaires:
+# Code developed by Florian Kuschel, Anna and David Pedrosa
+
+# Version 1.0 # 2024-09-12, # tidy up code and bring up potential ideas
+
+# TODO: This part is is a nice overview and may serve as a starting point for further ideas. But you  may consider tidying up everything a little. Especially it could make sense to create a single table one (code provided at the end)
+
+## ==========
+# Summarise sociodemographic data
+describe_vars <- c( # Variables to summarize
+  "age",
+  "years_since_diagnosis",
+  "UPDRS_I_Score",
+  "UPDRS_II_Score",
+  "FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_1_Psychiater",
+  "FIMA_1_Internist_FA", "FIMA_1_Gynaekologe", "FIMA_1_Urologe",
+  "FIMA_1_Orthopaede", "FIMA_1_Notfall_KH", "FIMA_1_other", "FIMA_1_Gesamt"
+) # TODO: further data may be added here
+
+# Function to summarize using describe
+summarize_describe <- function(variable) {
+  data <- df_safepd[[variable]]
+  desc <- psych::describe(data, IQR = TRUE)
+ 
+  # Convert describe output to a data frame
+  data.frame(
+    Variable = variable,
+    N = desc$n,
+    Mean = desc$mean,
+    SD = desc$sd,
+    Median = desc$median,
+    Min = desc$min,
+    Max = desc$max,
+    IQR = desc$IQR
+  )
+}
+
+# Apply the summarization function to all variables
+describe_results <- do.call(rbind, lapply(describe_vars, summarize_describe))
+
+## ========== your version
 # Soziodemographische Daten
 describe(df_safepd$age, IQR = TRUE)
 describe(subset(df_safepd$age, df_safepd$gender == 1))
@@ -11,7 +52,7 @@ VarDemographic <- c("age", "gender", "nationality", "martial_status", "years_sin
 
 Table_SozDem <- CreateCatTable(
   vars = VarDemographic,
-  strata = "gender", 
+  strata = "gender",
   data = df_safepd,
   includeNA = FALSE,
   test = TRUE,
@@ -27,7 +68,7 @@ VarDemographic_Group <- c("age", "gender_Group", "nationality_Group", "martial_s
 
 Table_SozDem_Group <- CreateCatTable(
   vars = VarDemographic_Group,
-  strata = "gender", 
+  strata = "gender",
   data = df_safepd,
   includeNA = TRUE,
   test = TRUE,
@@ -39,10 +80,10 @@ Table_SozDem_Group <- CreateCatTable(
   addOverall = TRUE
 )
 
-# Sicherheitsfragebogen 
+# Sicherheitsfragebogen
 Table_df_safepdQA1 <- CreateCatTable(
   vars = "overall_situation_Group",
-  strata = "UPDRS_I_Score", 
+  strata = "UPDRS_I_Score",
   data = df_safepd,
   includeNA = TRUE,
   test = TRUE,
@@ -54,72 +95,8 @@ Table_df_safepdQA1 <- CreateCatTable(
   addOverall = TRUE
 )
 
-# UPDRS
-describe(df_safepd$UPDRS_I_Score, IQR = TRUE)
-describe(subset(df_safepd$UPDRS_I_Score, df_safepd$gender == 1))
-describe(subset(df_safepd$UPDRS_I_Score, df_safepd$gender == 0))
-
-describe(df_safepd$UPDRS_II_Score, IQR = TRUE)
-describe(subset(df_safepd$UPDRS_II_Score, df_safepd$gender == 1))
-describe(subset(df_safepd$UPDRS_II_Score, df_safepd$gender == 0))
-
-VarUPDRS <- c("UPDRS_1_7",	"UPDRS_1_8",	"UPDRS_1_9",	"UPDRS_1_10",	"UPDRS_1_11",	"UPDRS_1_12",	"UPDRS_1_13", "UPDRS_2_1", "UPDRS_2_2",	"UPDRS_2_3",	"UPDRS_2_4",	"UPDRS_2_5",	"UPDRS_2_6",	"UPDRS_2_7",	"UPDRS_2_8",	"UPDRS_2_9",	"UPDRS_2_10",	"UPDRS_2_11",	"UPDRS_2_12",	"UPDRS_2_13")
-Table_UPDRS_All <- CreateCatTable(
-  vars = VarUPDRS,
-  strata = "gender",
-  data = df_safepd,
-  includeNA = TRUE,
-  test = TRUE,
-  testApprox = chisq.test,
-  argsApprox = list(correct = TRUE),
-  testExact = fisher.test,
-  argsExact = list(workspace = 2 * 10^5),
-  smd = TRUE,
-  addOverall = TRUE
-) 
-
-Table_UPDRS_I <- CreateCatTable( 
-  vars = "UPDRS_I_Score",
-  strata = "gender",
-  data = df_safepd,
-  includeNA = TRUE,
-  test = TRUE,
-  testApprox = chisq.test,
-  argsApprox = list(correct = TRUE),
-  testExact = fisher.test,
-  argsExact = list(workspace = 2 * 10^5),
-  smd = TRUE,
-  addOverall = TRUE
-)
-
-Table_UPDRS_II <- CreateCatTable( 
-  vars = "UPDRS_II_Score",
-  strata = "gender",
-  data = df_safepd,
-  includeNA = TRUE,
-  test = TRUE,
-  testApprox = chisq.test,
-  argsApprox = list(correct = TRUE),
-  testExact = fisher.test,
-  argsExact = list(workspace = 2 * 10^5),
-  smd = TRUE,
-  addOverall = TRUE
-) 
-
-# FIMA
-describe(df_safepd$FIMA_1_Hausarzt, IQR = TRUE)
-describe(df_safepd$FIMA_1_Neurologe, IQR = TRUE)
-describe(df_safepd$FIMA_1_Psychiater, IQR = TRUE)
-describe(df_safepd$FIMA_1_Internist_FA, IQR = TRUE)
-describe(subset(df_safepd$FIMA_1_Gynaekologe, df_safepd$gender == 0), IQR = TRUE)
-describe(subset(df_safepd$FIMA_1_Urologe, df_safepd$gender == 1),IQR = TRUE)
-describe(df_safepd$FIMA_1_Orthopaede, IQR = TRUE)
-describe(df_safepd$FIMA_1_Notfall_KH, IQR = TRUE)
-describe(df_safepd$FIMA_1_other, IQR = TRUE)
-describe(df_safepd$FIMA_1_Gesamt, IQR = TRUE)
-
 VarArztbesuche <- c("FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_1_Psychiater", "FIMA_1_Internist_FA", "FIMA_1_Gynaekologe", "FIMA_1_Urologe", "FIMA_1_Orthopaede", "FIMA_1_Notfall_KH", "FIMA_1_other")
-Table_FIMA1 <- CreateCatTable( 
+Table_FIMA1 <- CreateCatTable(
   vars = VarArztbesuche,
   strata = "gender",
   data = df_safepd,
@@ -131,10 +108,10 @@ Table_FIMA1 <- CreateCatTable(
   argsExact = list(workspace = 2 * 10^5),
   smd = TRUE,
   addOverall = TRUE
-) 
+)
 
 VarTherapie <- c("FIMA_2_Krankengymnastik", "FIMA_2_Ergotherapie", "FIMA_2_Sprachtherapie", "FIMA_2_Heilpraktiker", "FIMA_2_Osteopath", "FIMA_2_Chiropraktiker", "FIMA_2_Psychotherapeut")
-Table_FIMA2 <- CreateCatTable( 
+Table_FIMA2 <- CreateCatTable(
   vars = VarTherapie,
   strata = "gender",
   data = df_safepd,
@@ -146,7 +123,7 @@ Table_FIMA2 <- CreateCatTable(
   argsExact = list(workspace = 2 * 10^5),
   smd = FALSE,
   addOverall = TRUE
-) 
+)
 
 VarPflege <- c("FIMA_3", "FIMA_4", "FIMA_5", "FIMA_6", "FIMA_7", "FIMA_8", "FIMA_8_Group")
 Table_FIMA3 <- CreateCatTable(
@@ -154,7 +131,7 @@ Table_FIMA3 <- CreateCatTable(
   strata = "gender",
   data = df_safepd,
   includeNA = TRUE,
-  test = TRUE, 
+  test = TRUE,
   testApprox = chisq.test,
   argsApprox = list(correct = TRUE),
   testExact = fisher.test,
@@ -162,14 +139,14 @@ Table_FIMA3 <- CreateCatTable(
   smd = FALSE,
   addOverall = TRUE
 )
-  
+ 
 VarKlinik <- c("FIMA_9_Group", "FIMA_10_Group", "FIMA_11_Group", "FIMA_12_Group")
 Table_FIMA4 <- CreateCatTable(
   vars = VarKlinik,
   strata = "gender",
   data = df_safepd,
   includeNA = TRUE,
-  test = TRUE, 
+  test = TRUE,
   testApprox = chisq.test,
   argsApprox = list(correct = TRUE),
   testExact = fisher.test,
@@ -177,11 +154,11 @@ Table_FIMA4 <- CreateCatTable(
   smd = FALSE,
   addOverall = TRUE
 )
-  
+ 
 VarWohnung <- c("FIMA_15", "FIMA_14_Group")
 Table_FIMA5 <- CreateCatTable(
   vars = VarWohnung,
-  strata = "gender", 
+  strata = "gender",
   data = df_safepd,
   includeNA = TRUE,
   test = TRUE,
@@ -192,10 +169,10 @@ Table_FIMA5 <- CreateCatTable(
   smd = FALSE,
   addOverall = TRUE
 )
-  
+ 
 Table_FIMA6 <- CreateCatTable(
   vars = "FIMA_16_Group",
-  strata = "gender", 
+  strata = "gender",
   data = df_safepd,
   includeNA = TRUE,
   test = TRUE,
@@ -208,4 +185,79 @@ Table_FIMA6 <- CreateCatTable(
 )
 
 
-  
+## ========== ALternative
+# Create TableOne (if you opt for this, I would consider moving this to a different filed called TableOne.R or similar)
+
+# 1.1: Helper function: recodes variables as factors
+recode_factors <- function(df, vars, levels_labels = NULL) {
+  df[vars] <- lapply(df[vars], function(x) factor(as.character(x)))
+  if (!is.null(levels_labels)) {
+    for (var in names(levels_labels)) {
+      df[[var]] <- factor(df[[var]], levels = levels_labels[[var]]$levels, labels = levels_labels[[var]]$labels)
+    }
+  }
+  return(df)
+}
+
+# Duplicates df_recoded as df_tableone for clarity
+df_tableone <- df_safepd
+
+# 1.2: Define specific factor levels and labels (TODO: The entire next part MUST be adapted to your data)
+levels_labels <- list(
+  sex = list(levels = c("1", "2", "3"), labels = c("Male", "Female", "Divers")),
+  age_r = list(levels = c("1", "2", "3"), labels = c("≤ 60 years", "61 - 69 years", "≥ 70 years")),
+  education_r_r = list(levels = c("1", "2", "3"), labels = c("Primary school", "Higher secondary school", "University degree or higher")),
+  financial_stability_r_r = list(levels = c("1", "2"), labels = c("Most of the time", "Some of the time or less")),
+  family_status_r_r = list(levels = c("1", "2"), labels = c("Without a partner", "With a partner")),
+  living_situation_r_r = list(levels = c("1", "2"), labels = c("Independently", "With professional support")),
+  living_area_r = list(levels = c("1", "2", "3"), labels = c("Large city", "Medium-small city", "Rural area")),
+ 
+  who5_depr_r = list(levels = c("0", "1"), labels = c("No depression", "Impaired quality of life")),
+  comorb_r = list(levels = c("1", "2"), labels = c("Not Present", "Present")),
+  time_from_diagnosis_r = list(levels = c("1", "2", "3", "4", "5"), labels = c("<2 years", "2-5 years", "5-10 years", "10-15 years", ">15 years")),
+  age_at_diagnosis_r = list(levels = c("1", "2"), labels = c("<50 years", "≥50 years")),
+  disturbances_sleep_APD = list(levels = c("0", "1"), labels = c("No", "Yes")),
+  ms_fluctuation_APD = list(levels = c("0", "1"), labels = c("No", "Yes")),
+  assist_mov_r = list(levels = c("0", "1"), labels = c("No assisted movement", "Assisted movement")),
+  falls_r = list(levels = c("1", "2"), labels = c("No", "Yes"))
+)
+
+df_tableone <- recode_factors(df_tableone, names(levels_labels), levels_labels) # TODO: once levels_labels is defined, you should run this line
+
+# 1.3: Select variables and create labels for TableOne
+Vars <- c("age", "age_r", "sex", "education_r_r", "financial_stability_r_r", "family_status_r_r",
+          "living_situation_r_r", "living_area_r", "who5_depr_r", "comorb_r", "age_at_diagnosis_r",
+          "time_from_diagnosis_r", "disturbances_sleep_APD", "ms_fluctuation_APD", "assist_mov_r", "falls_r")
+
+colnames_Vars <- c("Age", "Age categorized", "Gender", "Education level", "Financial stability",
+                   "Family status", "Household situation", "Living area", "Depression", "Comorbidity",
+                   "Age at diagnosis", "Time since diagnosis", "Sleep disturbances",
+                   "Motor symptoms fluctuation", "Assisted movement", "Falls")
+
+# 1.4: TableOne function
+create_table_one <- function(df, vars, colnames_vars, factor_vars) {
+  df_tableOne <- df %>% dplyr::select(all_of(vars))
+  colnames(df_tableOne) <- colnames_vars
+  results.tableOne <- CreateTableOne(vars = colnames_vars, factorVars = factor_vars, data = df_tableOne)
+  print(results.tableOne, nonnormal = c("Time since diagnosis", "Education level"), showAllLevels = TRUE)
+  return(results.tableOne)
+}
+
+# Create the TableOne and display
+factVars <- colnames_Vars[-c(1, 2)]  # Exclude continuous variables
+results.tableOne <- create_table_one(df_tableone, Vars, colnames_Vars, factVars)
+print(head(df_tableone))
+
+# 1.5: Export TableOne to MS Excel file
+export_table_one <- function(results, filename = "Table1.xlsx") {
+  tableOne_matrix <- print(results, nonnormal = c("Time since diagnosis", "Education level"), showAllLevels = TRUE)
+  tableOne_with_rowname <- cbind(Variable = rownames(tableOne_matrix), tableOne_matrix)
+ 
+  wb1 <- createWorkbook()
+  addWorksheet(wb1, "Table1")
+  writeData(wb1, "Table1", tableOne_with_rowname)
+  setColWidths(wb1, "Table1", cols = 1:ncol(tableOne_with_rowname), widths = "auto")
+  saveWorkbook(wb1, file = file.path(getwd(), "results", filename), overwrite = TRUE)
+}
+
+export_table_one(results.tableOne)
