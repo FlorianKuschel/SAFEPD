3597d5ea11e6af27dced1b388fd3c57ae6eeb897
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2025-02-19 22:01:46 +0100

    Update SAFEPD_imputation.R

--- SAFEPD_imputation.R
+++ SAFEPD_imputation.R
@@ -1,6 +1,7 @@
 # Imputation of missing variables/input if feasible
 # Code developed by Florian Kuschel, Anna and David Pedrosa
 # Version 1.0 # 2024-09-12, # first draft of code adapted from distinct repository (https://www.github.com/dpedrosac/qol_prospective/)
+# Version 2.0 # 2025-19-01, # small change so only "inlist"-variables are considered for imputation
 
 # ================================================================================================== 
 # 1. Preparing data before imputation
@@ -39,35 +40,32 @@ text(
 
 dev.off() # Close the PDF device to save the file
 
-# TODO: for the time being, everything is considered. ALthough it makes no sense to impute every variable
-# necessarily. e.g. if there are hardly any numbers outside a specific range such as in nationality
-# TODO: I tidied up the variables for imputation
 # ================================================================================================== 
 # 2. Start imputation
 # ================================================================================================== 
 # Define the variables to be included as covariates in each imputation model
-inlist <- c("age", "years_since_diagnosis", "gender",
+inlist <- c("age", "years_since_diagnosis", "gender", "nationality",
             "martial_status", "school_graduation", "persons_houshold", 
             "professional_graduation", "employment_status", 
             "lack_of_information",  "uncertain_future",  "chaging_symptom_severity",  "gait_insecurity_fall",  
             "pain",  "gastrointestinal_symptoms",  "urinary_symptoms", "mental_abilities", "mental_symptoms", "other_disease",  "nursing_care",  "side_effects_complications",  "access_healthcare",
             "communication_with_me",  "communication_between_professionals",  "loneliness",  "everyday_problems",
             "daily_routine",  "overload_among_people",  "pejorativ_looks_comments",  "family_role",  
-            "conflicts_with_relatives",  "victim_to_crime",  "not_at_peace_with_myself",  
+            "conflicts_with_relatives",  "victim_to_crime", "financial_worries", "not_at_peace_with_myself",  
             "participation_in_road_traffic",  "overall_situation", 
             "UPDRS_I_Score", "UPDRS_II_Score", "FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_2_Krankengymnastik"
 )
 
 # Generate predictor matrix with minimum proportion of usable cases set to 0.5
+df_for_imputation <- df_before_imputation[, inlist]
 pred <- quickpred(
-  df_before_imputation,
-  minpuc = 0.5,
-  include = inlist
+  df_for_imputation,
+  minpuc = 0.5
 )
 
 # Perform multiple imputation using the MICE package
 generate_imputation <- mice(
-  data = df_before_imputation,
+  data = df_for_imputation,
   predictorMatrix = pred,
   method = "midastouch", # Imputation method
   m = 10,                # Number of imputed datasets
@@ -86,8 +84,14 @@ densityplot(
 
 dev.off()  # Close the PDF device to save the file
 
-# Save data after imputation
-imputed_data <- complete(generate_imputation, 2)
+# Save data after imputation as imputed values
+imputed_values <- complete(generate_imputation, 1)
+
+# Save data after imputation in original dataset
+imputed_data <- df_before_imputation  
+imputed_data[inlist] <- imputed_values  
+
+# Visualize missing data after imputation
 if (flag_check) {
   aggr_plot <- aggr(
     imputed_data,
@@ -100,3 +104,5 @@ if (flag_check) {
     ylab = c("Histogram of missing data", "Pattern")
   )
 }
+
+        
